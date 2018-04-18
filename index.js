4e45aac3af932d920c990415f45ce81827cfb95a
Levhita <levhita@gmail.com>
2018-04-18 14:15:22 -0500

    proof of concept

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function() {
   let Schedule= new scheduleModel();
   Schedule.load();
@@ -9,35 +8,11 @@ $(document).ready(function() {
       center: "title",
       right: "month,basicWeek,basicDay"
     },
-    defaultDate: "2018-03-12",
+    //defaultDate: "2018-03-12",
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     eventLimit: true, // allow "more" link when too many events
     events: Schedule.getEvents()
   });
-
-
-
 });
 
-  var tasks = [
-    {id: 1, duration: 60, name:'one'},
-    {id: 2, duration: 60, available: later.parse.text('every weekday'), name:'one'},
-    {id: 3, duration: 30, dependsOn: [1], priority: 100, name:'two'},
-    {id: 4, duration: 30, dependsOn: [1], minLength: 30, name:'three'},
-    {id: 5, duration: 90, dependsOn: [2,3], name:'four'},
-    {id: 6, duration: 45, dependsOn: [4], resources: ['A'], name:'five'},
-    {id: 7, duration: 60, dependsOn: [5], resources: ['A', 'B'], name:'six'},
-    {id: 8, duration: 30, dependsOn: [6,7], resources: [['A', 'B'], 'C'], name:'seven'}
-  ];
-
-    var resources = [
-    {id: 'A'},
-    {id: 'B', available: later.parse.text('after 10:00am and before 6:00pm')},
-    {id: 'C', isNotReservable: true}
-  ];
-
-  var projectAvailability = later.parse.text('every weekday'),
-  startDate = new Date();
-
-  var s = schedule.create(tasks, resources, projectAvailability, startDate);
--- scheduleModel.js
+++ scheduleModel.js
@@ -1,64 +1,66 @@
 class scheduleModel {
   load() {
-    this.events = [
-      {
-        title: "All Day Event",
-        start: "2018-03-01"
-      },
-      {
-        title: "Long Event",
-        start: "2018-03-07",
-        end: "2018-03-10"
-      },
-      {
-        id: 999,
-        title: "Repeating Event",
-        start: "2018-03-09T16:00:00"
-      },
-      {
-        id: 999,
-        title: "Repeating Event",
-        start: "2018-03-16T16:00:00"
-      },
-      {
-        title: "Conference",
-        start: "2018-03-11",
-        end: "2018-03-13"
-      },
-      {
-        title: "Meeting",
-        start: "2018-03-12T10:30:00",
-        end: "2018-03-12T12:30:00"
-      },
-      {
-        title: "Lunch",
-        start: "2018-03-12T12:00:00"
-      },
-      {
-        title: "Meeting",
-        start: "2018-03-12T14:30:00"
-      },
-      {
-        title: "Happy Hour",
-        start: "2018-03-12T17:30:00"
-      },
-      {
-        title: "Dinner",
-        start: "2018-03-12T20:00:00"
-      },
-      {
-        title: "Birthday Party",
-        start: "2018-03-13T07:00:00"
-      },
-      {
-        title: "Click for Google",
-        url: "http://google.com/",
-        start: "2018-03-28"
+    this.loadMockup();
+    console.log(this.scheduledTasks);
+    this.events=[];
+    for(let id in this.scheduledTasks) {
+      let task = this.scheduledTasks[id];
+      
+
+      for(let i=0;i<task.schedule.length;i++){
+        let start = new Date(task.schedule[i].start);
+        let end = new Date(task.schedule[i].end);
+
+        let event = {
+            id: id,
+            title: this.tasksData[id],
+            start: start.toISOString(),
+            end: end.toISOString(),
+        }
+        console.log(event);
+        this.events.push(event);
       }
-    ];
+    }
   }
 
   getEvents() {
     return this.events;
+  }
+
+  loadMockup() {
+    this.tasksData= {
+      1: 'Presentation',
+      2: 'English Class',
+      3: 'Priority Class',
+      4: 'Minimal 2 Hours',
+      5: 'Doubly Dependent Task 1',
+      6: 'Another Task',
+      7: 'Yet Another Task',
+      8: 'I really dont understand this'
+    }
+
+    this.tasks = [
+    {id: 1, duration: 4*60, resources: ['class']},
+    {id: 2, duration: 1*60, available: later.parse.text('on monday after 8:00am and before 10:00am') ,resources: ['class']},
+    {id: 3, duration: 2*60, dependsOn: [1], priority: 100, minSchedule: 120, resources: ['class']},
+    {id: 4, duration: 8*60, dependsOn: [1], minSchedule: 120 ,resources: ['class']},
+    {id: 5, duration: 5*60, dependsOn: [1,3] ,resources: ['class']},
+    {id: 6, duration: 3*60, dependsOn: [4], resources: ['class'] },
+    {id: 7, duration: 3*60, dependsOn: [5],  resources: ['class'] },
+    {id: 8, duration: 3*60, dependsOn: [6,7], resources: ['class'] }
+    ];
+
+    this.resources = [
+    {id: 'A'},
+    {id: 'class', available: later.parse.text('after 9:00am and before 2:30pm')},
+    {id: 'C', isNotReservable: true}
+    ];
+
+    this.projectAvailability = later.parse.text('every weekday after 9:00am and before 2:00pm'),
+    this.startDate = new Date();
+    
+    schedule.date.localTime();
+    this.scheduledTasks = schedule.create(this.tasks, this.resources, this.projectAvailability, this.startDate).scheduledTasks;
+
   }
 }
